3f9f197e959e542fb1b6aea1c22df5cbfed4d13c
Yuyi <yuyi365@gmail.com>
2021-12-15 10:38:36 -0500

    commit tree 8e35b457d2aed339a7d681a7ef4215d36500bdd9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(items_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found
+    render json: { error: 'review not found' }, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :description, :price)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  wrap_parameters false
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
   end
 
+  private
+
+  def render_not_found
+    render json: { error: 'user not found' }, status: :not_found
+  end
 end
